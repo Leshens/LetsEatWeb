e54983b891280927fcd27853af9a2a0ed855d170
Northberth <norbertm398@gmail.com>
2023-10-26 19:03:59 +0200

    update v2

--- src/App.js
+++ src/App.js
@@ -1,55 +1,22 @@
-import logo from './img/logoLE2.png';
-import table from './img/table.png';
-import person from './img/person.png';
-import AppStore from './img/AppStore.png';
-import GooglePlay from './img/GooglePlay.png';
-import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/Home";
 
 
-
-function App() {
+export default function App() {
   
   return (
-    <div className="App">
-      <script>
-      function Login(){
-    <a href="/loginPage.js" target="_blank" rel="noopener noreferrer"></a> 
-    }
-      </script>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <div className="loginButtons">
-        <button class="login" onclick="window.open('/loginPage.js','_blank'">Zaloguj się</button>
-        <br></br>
-        <button class="signup" onclick="window.open('/loginPage.js','_blank'">Zarejestruj się</button>
-      </div>
-      <div className="Download">
-        <img src={AppStore} className="AppStore" alt="AppStore" />
-        <img src={GooglePlay} className="GooglePlay" alt="GooglePlay" />
-      </div>
-      </header>
-
-      <div className="MainText">
-        Z nami <br></br>znajdziesz<br></br>wolny stolik
-      </div>
-
-      <img src={table} className="table" alt="table" />
-      <img src={person} className="person" alt="person" />
-
-      <div className="line"></div>
-
-      <div className="SecondText">
-        Chcesz znaleźć wolne miejsce w<br></br>pobliskiej restauracji? Z nami jest<br></br>to banalnie proste!<br></br>
-      </div>
-
-      <div className="ThirdText">
-      Zacznij już teraz.
-      </div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
-
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import logo from '../img/logoLE2.png';
+import table from '../img/table.png';
+import person from '../img/person.png';
+import AppStore from '../img/AppStore.png';
+import GooglePlay from '../img/GooglePlay.png';
+import '../App.css';
+
+
+const Home = () => {
+  return <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+  <div className="loginButtons">
+    <button class="login" onclick="window.open('/loginPage.js','_blank'">Zaloguj się</button>
+    <br></br>
+    <button class="signup" onclick="window.open('/loginPage.js','_blank'">Zarejestruj się</button>
+  </div>
+  <div className="Download">
+    <img src={AppStore} className="AppStore" alt="AppStore" />
+    <img src={GooglePlay} className="GooglePlay" alt="GooglePlay" />
+  </div>
+  </header>
+    <div className="MainText">
+      Z nami <br></br>znajdziesz<br></br>wolny stolik
+    </div>
+
+    <img src={table} className="table" alt="table" />
+    <img src={person} className="person" alt="person" />
+
+    <div className="line"></div>
+
+    <div className="SecondText">
+      Chcesz znaleźć wolne miejsce w<br></br>pobliskiej restauracji? Z nami jest<br></br>to banalnie proste!<br></br>
+    </div>
+
+    <div className="ThirdText">
+    Zacznij już teraz.
+    </div>
+  </div>;
+};
+
+export default Home;
+
