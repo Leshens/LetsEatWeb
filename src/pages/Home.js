534b3bbb454fb20aea9f9ec447cad8cafa38ee30
kharass <k.haras@wp.pl>
2023-10-30 22:20:43 +0100

    home - tailwind zrobiony na picus glancus idealnie
    Karolina

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,37 +7,54 @@ import '../App.css';
 
 const Home = () => {
   return <div className="text-center h-full">
-  <header className="App-header">
+
+{/* przyciski pobierania */}
+<div className="Download">
+    <img src={AppStore} className="lg:h-12 md:h-10 sm:h-8 sx:h-6 inline-flex"  alt="AppStore" />
+    <div className="lg:w-10 md:w-10 sm:w-8 sx:w-6 inline-flex"></div>
+    <img src={GooglePlay} className="lg:h-12 md:h-10 sm:h-8 sx:h-6 inline-flex" alt="GooglePlay" />
+  </div>
+
+<header className="App-header">
+{/* przerwa żeby download byl dalej od main textu */}
+  <div className="h-24 w-24">
+  </div>
+
+    {/* logo */}
     <img src={logo} className="lg:h-36 md:h-28 sm:h-20 sx:h-16 top-4 left-4 absolute" alt="logo" />
+
+  {/* przyciski logowania/rejestrowania */}
   <div className="loginButtons">
     <a href = "/Login">
-    <button type= "button" class="bg-primary text-black p-4 text-center flex lg:text-xl md:text-lg sm:text-base sx:text-xs float-right rounded-3xl font-extrabold top-2.5 right-5 absolute">Zaloguj się</button>
+    <button type= "button" class="bg-primary text-black lg:px-12 md:px-10 sm:px-8 sx:px-6  lg:p-4 md:p-3 sm:p-2 sx:p-1.5 text-center flex lg:text-xl md:text-lg sm:text-base sx:text-xs float-right rounded-full font-extrabold top-2.5 right-5 absolute">Zaloguj się</button>
     </a>
     <br></br>
     <a href = "/Signup">
-    <button type="button" class="bg-primary text-black p-3 text-center flex lg:text-xl md:text-lg sm:text-base sx:text-xs float-right rounded-3xl font-extrabold top-20 right-9 absolute">Zarejestruj się</button>
+    <button type="button" class="bg-primary text-black lg:p-3 md:p-2 sm:p-1.5 sx:p-1 text-center flex lg:text-xl md:text-lg sm:text-base sx:text-xs float-right rounded-full font-extrabold lg:top-20 md:top-20 sm:top-16 sx:top-12  right-8 absolute">Zarejestruj się</button>
     </a>
   </div>
-  <div className="Download">
-    <img src={AppStore} className="lg:h-12 md:h-10 sm:h-8 sx:h-6 top-2.5 right-1/4 inline-flex items-end justify-end"  alt="AppStore" />
-    <img src={GooglePlay} className="lg:h-12 md:h-10 sm:h-8 sx:h-6 top-2.5 right-1/8 inline-flex items-end justify-end" alt="GooglePlay" />
-  </div>
-  </header>
+</header>
+
+    {/* main text */}
     <div className="lg:text-7xl md:text-6xl sm:text-4xl sx:text-3xl font-bold inline-flex items-center justify-center order-1 ">
       Z nami <br></br>znajdziesz<br></br>wolny stolik
       <img src={MainPicture} className="lg:h-52 md:h-40 sm:h-28 sx:h-20 inline-flex order-2" alt="MainPicture" />
     </div>
 
+    <div className="bg-primary lg:h-1 md:h-three sm:h-three sx:h-three  lg:w-3/4 md:w-3/4 sm:w-3/4 sx:w-3/4 inline-flex items-center justify-center"></div>
 
-    <div className="bg-primary lg:h-1 md:h-three sm:h-0.5 md:w-3/4 sm:w-3/4 sx: inline-flex items-center justify-center"></div>
+    {/* przerwa żeby linia byla dalej od dolnego textu */}
+    <div className="h-20 w-20">
+    </div>
 
+    {/* dolny text */}
     <div className="text-center lg:text-2xl md:text-xl sm:text-base">
       Chcesz znaleźć wolne miejsce w<br></br>pobliskiej restauracji? Z nami jest<br></br>to banalnie proste!<br></br>
     </div>
-
     <div className="text-center lg:text-2xl md:text-xl sm:text-base font-bold">
     Zacznij już teraz.
     </div>
+
   </div>;
 };
 
