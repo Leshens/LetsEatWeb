5833921959edc1a5b73fa14d6dcb71907468cd1d
Northberth <norbertm398@gmail.com>
2024-01-20 17:33:24 +0100

    Redirect and RestaurantForm
    dodano przekierowanie dla zalogowanych użytkowników do AdminMenu i dodano dodawanie restauracji z RestaurantForm

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import HomePresenter from "./pages/Home";
 import LoginPresenter from "./pages/Login";
 import SignupPresenter from "./pages/SignUp";
 import RestaurantFormPresenter from "./pages/RestaurantForm";
-import AdminMenuPresenter from "./pages/AdminMenu"
+import AdminMenuPresenter from "./pages/AdminMenu";
 import MenuTablePresenter from "./pages/MenuTable";
 import Protected from "./components/Protected";
 import StolikiTablePresenter from "./pages/StolikiTable";
 
 export default function App() {
+  const userInLocalStorage = localStorage.getItem("user");
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<HomePresenter/>}/>
-        <Route path='/Login' element={<LoginPresenter/>}/>
-        <Route path='/SignUp' element={<SignupPresenter/>}/>
-          <Route path="/" element={<Protected/>}>
-              <Route path='/' element={<Navbar />}/>
-              <Route path='/RestaurantForm' element={<RestaurantFormPresenter/>}/>
-              <Route path='/AdminMenu' element={<AdminMenuPresenter/>}/>
-              <Route path='/MenuTable' element={<MenuTablePresenter/>}/>
-              <Route path='/StolikiTable' element={<StolikiTablePresenter/>}/>
+          {userInLocalStorage ? (
+            <Route path="/" element={<Navigate to="/AdminMenu" />} />
+          ) : (
+            <>
+              <Route path="/" element={<HomePresenter />} />
+              <Route path="/Login" element={<LoginPresenter />} />
+              <Route path="/SignUp" element={<SignupPresenter />} />
+            </>
+          )}
+
+          <Route path="/" element={<Protected />}>
+            <Route path="/" element={<Navbar />} />
+            <Route path="/RestaurantForm" element={<RestaurantFormPresenter />} />
+            <Route path="/AdminMenu" element={<AdminMenuPresenter />} />
+            <Route path="/MenuTable" element={<MenuTablePresenter />} />
+            <Route path="/StolikiTable" element={<StolikiTablePresenter />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </div>
-    
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
--- src/pages/RestaurantForm.js
+++ src/pages/RestaurantForm.js
@@ -1,116 +1,148 @@
 import '../App.css';
 import logo from '../img/logoLE2.png';
 import { Formik } from "formik";
-import Dropdown from "./Dropdown"; 
+import Dropdown from "./Dropdown";
+import axios from 'axios';
 
 export default function RestaurantForm() {
     const options = [
-        { value: "ITALIAN", label: "Włoskie"},
-        { value: "CHINESE", label: "Chińskie"},
-        { value: "MEXICAN", label: "Meksykańskie"},
-        { value: "AMERICAN", label: "Amerykańskie"},
-        { value: "INDIAN", label: "Indyjskie"},
-        { value: "JAPANESE", label: "Japońskie"},
-        { value: "FRENCH", label: "Francuskie"},
-        { value: "OTHER", label: "Inne"},
-    ]
+        { value: "ITALIAN", label: "Włoskie" },
+        { value: "CHINESE", label: "Chińskie" },
+        { value: "MEXICAN", label: "Meksykańskie" },
+        { value: "AMERICAN", label: "Amerykańskie" },
+        { value: "INDIAN", label: "Indyjskie" },
+        { value: "JAPANESE", label: "Japońskie" },
+        { value: "FRENCH", label: "Francuskie" },
+        { value: "OTHER", label: "Inne" },
+    ];
+
+    const handleFormSubmit = async (values) => {
+        try {
+            const response = await axios.post('http://31.179.139.182:690/api/restaurants', values);
+            console.log('Restaurant added successfully:', response.data);
+        } catch (error) {
+            console.error('Error adding restaurant:', error);
+        }
+    };
+
     return (
         <div className="restaurantForm flex flex-col items-center justify-center">
 
-            {/* przerwa */}
             <div className="h-10 w-10"></div>
 
             <p className="text-3xl text-secondary font-semibold text-center">
                 Wypełnij formularz poniżej, żeby zacząć z nami współpracować
             </p>
 
-            {/* logo */}
             <a href="/">
-            <img src={logo} className="lg:h-36 md:h-28 sm:h-20 sx:h-16 top-4 left-4 absolute" alt="logo" />
+                <img src={logo} className="lg:h-36 md:h-28 sm:h-20 sx:h-16 top-4 left-4 absolute" alt="logo" />
             </a>
 
-            {/* przerwa */}
             <div className="h-10 w-10"></div>
 
-            {/* przerwa */}
             <div className="h-4 w-10"></div>
 
-        <div className="border-primary border-2 rounded-3xl px-12 flex">
-            <Formik>
+            <div className="border-primary border-2 rounded-3xl px-12 flex">
+                <Formik
+                    initialValues={{
+                        'restaurantName': '',
+                        'restaurantCategory': '',
+                        'openingHours': '',
+                        'adress': '',
+                        'phoneNumber': '',
+                        'photoLink': '',
+                        'websiteLink': '',
+                    }}
+                    onSubmit={handleFormSubmit}
+                >
+                    {({ values, handleChange, handleSubmit }) => (
+                        <div className="form">
+                            <div className="h-8 w-10"></div>
 
-            <div className="form">
+                            <input
+                                type="string"
+                                name="restaurantName"
+                                placeholder="nazwa restauracji"
+                                className="form-control"
+                                value={values['restaurantName']}
+                                onChange={handleChange}
+                            />
+                            <br></br>
+                            <div className="Dropdown text-secondary">
+                                <Dropdown
+                                    isMulti
+                                    placeHolder="Wybierz Rodzaj Restauracji..."
+                                    options={options}
+                                    value={values['restaurantCategory']}
+                                    onChange={(selectedOption) => handleChange({ target: { name: 'restaurantCategory', value: selectedOption } })}
+                                />
+                            </div>
+                            <br></br>
+                            <input
+                                type="string"
+                                name="openingHours"
+                                placeholder="Godziny otwarcia"
+                                className="form-control"
+                                value={values['openingHours']}
+                                onChange={handleChange}
+                            />
+                            <br></br>
+                            <input
+                                type="string"
+                                name="adress"
+                                placeholder="adres"
+                                className="form-control"
+                                value={values['adress']}
+                                onChange={handleChange}
+                            />
+                            <br></br>
+                            <input
+                                type="string"
+                                name="phoneNumber"
+                                placeholder="numer telefonu"
+                                className="form-control"
+                                value={values['phoneNumber']}
+                                onChange={handleChange}
+                            />
+                            <br></br>
+                            <input
+                                type="string"
+                                name="photoLink"
+                                placeholder="Link do zdjęcia restauracji"
+                                className="form-control"
+                                value={values['photoLink']}
+                                onChange={handleChange}
+                            />
+                            <br></br>
+                            <input
+                                type="string"
+                                name="websiteLink"
+                                placeholder="Link do strony internetowej"
+                                className="form-control"
+                                value={values['websiteLink']}
+                                onChange={handleChange}
+                            />
+                            <br></br>
 
-            {/* przerwa */}
-            <div className="h-8 w-10"></div>
+                            <div className="h-10 w-10"></div>
 
-               <input
-                type="string"
-                name="restaurant-name"
-                placeholder="nazwa restauracji"
-                className="form-control"
-               />
-               <br></br>
-               <div className="Dropdown text-secondary">
-                    <Dropdown isMulti placeHolder="Wybierz Rodzaj Restauracji..." options={options} />
-                </div>
-        
-               <input
-                type="string"
-                name="openingHours"
-                placeholder="Godziny otwarcia: np.8AM-10PM"
-                className="form-control"
-               />
-               <br></br>
-               <input
-                type="string"
-                name="adress"
-                placeholder="adres"
-                className="form-control"
-               />
-               <br></br>
-               <input
-                type="string"
-                name="phoneNumber"
-                placeholder="numer telefonu"
-                className="form-control"
-               />
-               <br></br>
-               <input
-                type="string"
-                name="photoLink"
-                placeholder="Link do zdjęcia restauracji"
-                className="form-control"
-               />
-               <br></br>
-               <input
-                type="string"
-                name="websiteLink"
-                placeholder="Link do strony internetowej"
-                className="form-control"
-               />
-               <br></br>
-               
+                            <button
+                                type="button"
+                                className="bg-primary text-black lg:px-12 md:px-10 sm:px-10 sx:px-10 lg:p-4 md:p-3 sm:p-3 sx:p-3 lg:text-xl md:text-lg sm:text-lg sx:text-lg rounded-full font-extrabold"
+                                onClick={handleSubmit}
+                            >
+                                Zakończ
+                            </button>
 
-                {/* przerwa */}
-                <div className="h-10 w-10"></div>
+                            <div className="h-11 w-10"></div>
 
-               <a href="AdminMenu">
-               <button type= "button" class="bg-primary text-black lg:px-12 md:px-10 sm:px-10 sx:px-10   lg:p-4 md:p-3 sm:p-3 sx:p-3   lg:text-xl md:text-lg sm:text-lg sx:text-lg rounded-full font-extrabold ">
-                Zakończ</button>
-               </a>
-
-               {/* przerwa */}
-                <div className="h-11 w-10"></div>
-
+                        </div>
+                    )}
+                </Formik>
             </div>
-            </Formik>
+
+            <div className="h-20 w-10"></div>
 
         </div>
-
-        {/* przerwa */}
-        <div className="h-20 w-10"></div>
-    
-    </div>
-    )
-  };
-  
+    );
+};
